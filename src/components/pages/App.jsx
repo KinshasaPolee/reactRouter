46de301ce37c010a0332110af9fad500dbd57b19
KinshasaPolee <kpolee1@gmail.com>
2024-02-15 14:29:55 -0500

    all files added

---
+++ src/components/pages/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+// Import Route and our components
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/currencies";
+import Main from "./pages/main";
+import Price from "./pages/price";
+import Nav from "./components/pages/nav";
+
+export default function App() {
+  // We will use the Route component, path attribute, and element attribute to specify each route.
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/currencies" element={<Currencies />} />
+        <Route path="/price" element={<Price />} />
+      </Routes>
+    </div>
+  );
+}
